2257b5be4954efcc08684a6ea12177a67e9f873f
Gia Huy <21520909@gm.uit.edu.vn>
2023-05-30 15:09:56 +0700

    fix bug

--- lib/loginScreen.dart
+++ lib/loginScreen.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'signupScreen.dart';
-import 'mainScreen.dart';
+import 'mainScreen/mainScreen.dart';
 import 'package:validators/validators.dart';
 
 class LoginScreen extends StatefulWidget {
